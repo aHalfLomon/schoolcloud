b4df0425f65033413fc28b70c948f52e75b7dba5
wang6966 <2066229586@qq.com>
2023-04-03 22:42:34 +0800

    ver1.0.1

---
+++ Getaway/src/main/java/geteway/getawayapplication.java
@@ -0,0 +1,15 @@
+package geteway;
+
+/**
+ * @Classname getawayapplication
+ * @Description TODO
+ * @Date 2023/4/3 22:39
+ * @Created by wang
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class getawayapplication {
+    public static void main(String[] args) {
+        SpringApplication.run(getawayapplication.class,args);
+    };
+}
